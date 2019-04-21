efe129a2e340418d3fa199b9cd075d799f566b1b
Kevin Lam <kevinlam.ucsb@gmail.com>
2019-04-21 15:16:01 -0700

    create about page

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+    <h2>About Me</h2>
+    <p>
+      Here's who I am so you know I'm not some crazy guy locked up in a basement. Also, since you know who I am now,
+      these are my biases and my nurture environment. So whatever I say, take it with a grain of salt and keep in mind the biases
+      I am conditioned to have.
+    </p>
+  </Layout>
+)
+
+export default AboutPage
