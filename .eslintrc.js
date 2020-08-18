800ad3270e42740a427b4ac353662994b7d29771
Kevin Lam <kevinlam.sf@gmail.com>
2020-08-18 14:51:27 -0700

    add more eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   "globals": {
     "fetch": false
   },
+  "env"" {
+    "browser": true,
+    "node": true,
+  }
   "extends": ["airbnb", "airbnb/hooks", "prettier", "prettier/react"]
 };
